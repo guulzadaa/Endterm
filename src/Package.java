26e5830a7ad0d3e40f85ee15a84eaf60c6ccad22
Abzal <166910313+Neklzedbnv@users.noreply.github.com>
2025-01-29 10:53:21 +0500

    blmo

---
+++ src/Package.java
@@ -0,0 +1,19 @@
+public abstract class Package {
+    private String name;
+    private double price;
+
+    public Package(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public abstract void getPackageDetail();
+}
